536b72fe4a8facc05227464d441c3ce5a73aac29
[chase friedman] <[chaseafriedman@gmail.com]>
2024-04-17 17:03:06 -0700

    made app functional

---
+++ README.md
@@ -0,0 +1,37 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  # README Generator
+
+  ## Description
+
+  Using node to create a professional README generator
+
+  ## Table of Contents
+  - Installation
+  - Usage
+  - How to Contribute
+  - Tests
+  - Questions
+
+  ## Installation
+
+  By downloading the folder
+
+  ## Usage
+
+  Open the terminal and type "node index.js" then answer the prompts
+
+  MIT license.
+
+  ## How to Contribute
+
+  Venmo @chasegetsbetter
+
+  ## Tests
+
+  Use the app and email me your thoughts
+
+  ## Questions
+
+  If you have any questions you can reach me at chaseafriedman@gmail.com.
+  My GitHub profile can be found at https://github.com/cfriedman2156
---
+++ index.js
@@ -0,0 +1,71 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How is it installed?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use it?"
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "How can people contribute?"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "How can people test it?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a lisence",
+        choices: ["Apache 2.0", "GNU General Public v3.0", "MIT", "Boost Software 1.0", "Creative Commons Zero 1.0 Universal", "No License"]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What's your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your email?"
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log("Creating README file!");
+        writeToFile("README.md", generateMarkdown({...inquirerResponses}))
+    })
+}
+
+// Function call to initialize app
+init();
